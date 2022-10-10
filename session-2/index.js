8e785719d5ba9f6a214efcbded977dddd88da0f1
Yetunde Animashawun <yanim87@gmail.com>
2022-10-10 18:51:52 +0100

    Task 1 unit 3 JS

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,5 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log ("Yetty was here")
+console.log(document.querySelector(' .about').innerHTML)
+console.log( 'All about:'),document.querySelectorAll(' .about')
