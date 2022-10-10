1bf9ca7162340d64080aaa6ba362924ce5520a6b
Yetunde Animashawun <yanim87@gmail.com>
2022-10-10 18:53:22 +0100

    Task 1 unit 3 JS

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,14 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+let name='Tanya';
+let age=10;
+let a = 5;
+const b = 10;
+console.log(a+b);
+a = 20;
+console.log (a+b);
+let c = a + b;
+console.log(c);
+a = 20;
+console.log(c);
+c+a+b
