e4f2b16bcdf8a3e50ce739cb2e3cd090ea19a879
Yetunde Animashawun <yanim87@gmail.com>
2022-10-12 19:57:51 +0100

    new console.log for division

--- session-3/index.js
+++ session-3/index.js
@@ -12,3 +12,24 @@ console.log(c);
 a = 20;
 console.log(c);
 c+a+b
+const UserName = "Tanya"
+function SayHey (){
+    console.log ('Hey'+ UserName);
+}
+
+function conversation () {
+    SayHey();
+    console.log ('How are you?');
+    console.log ('Goodbye');
+    return 'Yetty was here'
+}
+conversation()
+
+ const currentAge = 35;
+ const calculatedAge = currentAge + 5;
+function futureAge () {
+    SayHey()+ name;
+    console.log ('You will be ' + calculatedAge + ' in 5 years time');
+    return 'Hi' name + ' You will be ' + calculatedAge + ' in 5 years time';
+}
+futureAge()
--- session-4/index.js
+++ session-4/index.js
@@ -1,9 +1,42 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+const personA = {
+    name: 'Amanda',
+    age: 24,
+    location: 'Cornwall',
+    likes: 'swimming',
+};
 
+const personB = {
+    name: 'Curtis',
+    age: 27,
+    location: 'Reading',
+    likes: 'football',
+};
 
+const personC = {
+    name: 'Layla',
+    age: 22,
+    location: 'Cambridge',
+    likes: 'poetry',
+};
 
+const personD = {
+    name: 'David',
+    age: 44,
+    location: 'Bath',
+    likes: 'rugby',
+};
 
-
+function biography(person)
+{
+    return "Hi my name is " + person.name + ". I am " + person.age + " years old, I live in "
+    + person.location + " and i like " + person.likes
+};
+console.log (biography(personA));
+console.log (biography(personB));
+console.log (biography(personC));
+console.log (biography(personD));
+ 
 
 
 
@@ -19,24 +52,30 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log(x * x);
+
+console.log(x + x + y);
+
+console.log (x * y / x);
+
+console.log("Division: x + y " + division);
 
 
 
