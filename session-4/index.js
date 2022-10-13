4a172bae1818828dd7ed870f56aaeca8c6dc62a5
Yetunde Animashawun <yanim87@gmail.com>
2022-10-13 17:39:11 +0100

    create calculator object

--- session-4/index.js
+++ session-4/index.js
@@ -52,40 +52,57 @@ console.log (biography(personD));
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-const x = 6;
-const y = 4;
+//const x = 6;
+// const y = 4;
 
-const addition = x + y;
+// const addition = x + y;
 
-console.log("Addition: x + y " + addition);
+// console.log("Addition: x + y " + addition);
 
-const subtraction = x - y;
+// const subtraction = x - y;
 
-console.log("Subtraction: x - y " + subtraction);
+// console.log("Subtraction: x - y " + subtraction);
 
-const multiplication = x * y;
+// const multiplication = x * y;
 
-console.log("multiplication: x * y " + multiplication);
+// console.log("multiplication: x * y " + multiplication);
 
-const division = x * y;
+// const division = x * y;
 
-console.log(x * x);
+// console.log(x * x);
 
-console.log(x + x + y);
+// console.log(x + x + y);
 
-console.log (x * y / x);
+// console.log (x * y / x);
 
-console.log("Division: x + y " + division);
+// console.log("Division: x + y " + division);
 
-const myAge = 35;
-const minimumDrivingAge = 18;
-const oldenoughtodrive = myAge > minimumDrivingAge;
+// const myAge = 35;
+// const minimumDrivingAge = 18;
+// const oldenoughtodrive = myAge > minimumDrivingAge;
 
-console.log ('Am I old enough to drive?' + oldenoughtodrive);
+// console.log ('Am I old enough to drive?' + oldenoughtodrive);
 
-
-
-
+function addNumber (x,y) {
+    return x + y
+}
+function subtractNumber (x,y) {
+    return x - y
+}
+const calculator = {
+    add: addNumber,
+    subtract: subtractNumber,
+    multiply: function multiplyNumber (x,y)
+    { return x * y} ,
+    
+    divide: function divideNumber (x,y) {
+        return x /y
+},
+}
+console.log (calculator.add(3,5));
+console.log (calculator.subtract(9,4));
+console.log (calculator.multiply(6,7));
+console.log (calculator.divide(46,2));
 
 
 
