ba34bae9bc1057f2390ce7beca5c358b7b3aa347
Yetunde Animashawun <yanim87@gmail.com>
2022-10-19 19:36:30 +0100

    for loop

--- session-6/index.js
+++ session-6/index.js
@@ -2,14 +2,20 @@
 
 let counter = 2;
 let randomNumber = 10;
-while (counter <= 10) {
-    const randomNumber = Math.round(Math.random() * 10);
+// while (counter <= 10) {
+//     const randomNumber = Math.round(Math.random() * 10);
 
-    counter = counter + 1;
+//     counter = counter + 1;
     
-}
-console.log ('while Random number: ' + randomNumber); 
+    
+// }
+// console.log ('while Random number: ' + randomNumber); 
 
+for (let counter = 2; counter <= 10; counter = counter +1) {
+    randomNumber = Math.round(Math.random() * 10);
+}
+
+console.log ('while Random number: ' + randomNumber);
 
 // ****************************************************TASK 1****************************************************
 
